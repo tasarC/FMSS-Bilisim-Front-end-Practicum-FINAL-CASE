41cb5e8840159fcc105febb46343e9abe361422a
Cennet Tasar <81915186+tasarC@users.noreply.github.com>
2023-04-30 01:13:36 +0300

    Update index.js

--- StarWars/src/index.js
+++ StarWars/src/index.js
@@ -5,10 +5,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-
+//Bu, React Query tarafından sunulan bir önbellek yöneticisi olan ve bir REST API'ye veri isteklerinde bulunmamızı sağlayan bir araçtır
 const queryClient = new QueryClient({
+  //defaultOptions içindeki queries ayarları, önbellek davranışlarını değiştirir.
   defaultOptions:{
     queries:{
+      //refetchOnMount ve refetchOnWindowFocus ayarları, bileşen ilk render edildiğinde ve pencere odak kaybettiğinde yeniden yükleme işlemlerinin gerçekleşmemesini sağlar
       refetchOnMount:false,
       refetchOnWindowFocus:false
     }
